fa87477b4529a6724e4f4562b293c287d2317884
Alanna Bouloy <lanihope@gmail.com>
2020-10-02 12:26:25 -0400

    added Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
 import slugify from 'slugify';
 
 import './App.css';
+import Header from './Header'
 
 // This object will allow us to
 // easily convert numbers into US dollar values
@@ -97,9 +97,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing | Laptops</h1>
-        </header>
+        <Header />
         <main>
           <form className="main__form">
             <h2>Customize your laptop</h2>
