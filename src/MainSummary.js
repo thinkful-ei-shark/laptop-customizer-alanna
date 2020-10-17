897d71e81d4f189ddbb456fc30c2af5b804945b4
Alanna Bouloy <lanihope@gmail.com>
2020-10-17 14:36:18 -0400

    added Total component

--- src/MainSummary.js
+++ src/MainSummary.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import SummaryItem from './SummaryItem';
+import Total from './Total';
 
 export default function MainSummary(props){
     return (
             <section className="main__summary">
                 <h2>Your cart</h2>
                 <SummaryItem currencyFormat={props.currencyFormat} selected = {props.selected}/>
-                <div className="summary__total">
-                <div className="summary__total__label">Total</div>
-                <div className="summary__total__value">
-                    {props.currencyFormat.format(props.total)}
-                </div>
-                </div>
+                <Total currencyFormat={props.currencyFormat} total={props.total}/>
           </section>
     )
 }
